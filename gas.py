8bab2d480c3b130eceb6918d29e131489f6e3c72
Daniel Lopez <danielalejandrol14@gmail.com>
2022-02-15 16:34:45 -0500

    Creando objeto gas

--- gas.py
+++ gas.py
@@ -1,2 +1,36 @@
+import numpy as np
+from Ball import *
+from ctypes.wintypes import RGB
+from tkinter import *
+from collections import namedtuple
+
 class Gas:
-    pass
+    def __init__(self, interaction, number_of_particles, volumen, window, canvas):
+        self.interaction = interaction 
+        self.number_of_particles = number_of_particles
+        self.volumen = volumen
+        self.window = window
+        self.canvas = canvas
+    
+    def create_gas(self):
+        max_x_velocity=20
+        max_y_velocity=20
+
+        radius = 5
+        Color = namedtuple('Color', 'red green blue')
+
+        initial_position_x = np.random.randint(10, self.volumen.width-1, self.number_of_particles)
+        x_velocity = np.random.randint(-max_x_velocity, max_x_velocity, self.number_of_particles)
+        initial_position_y = np.random.randint(10, self.volumen.height-1, self.number_of_particles)
+        y_velocity = np.random.randint(-max_y_velocity,max_y_velocity, self.number_of_particles)
+
+        colors_depending_on_velocity = lambda x_vel, y_vel: Color(round((x_vel**2 + y_vel**2)**0.5*255/((max_y_velocity**2+max_x_velocity**2)**0.5)),139,116)
+
+        particles=[Ball(self.canvas, initial_position_x[i], initial_position_y[i],
+            x_velocity[i], y_velocity[i], radius, colors_depending_on_velocity(x_velocity[i],y_velocity[i])) for i in range(self.number_of_particles)]
+
+        return particles
+
+    def move_gas(self, particles):
+        for i in range(self.number_of_particles):
+            particles[i].move()
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from ctypes.wintypes import RGB
 from tkinter import *
 import time
 from Ball import *
+from Gas import *
 import numpy as np
 from collections import namedtuple
 
@@ -9,40 +10,26 @@ def run():
 
     window = Tk()
 
-    WIDTH=600
-    HEIGHT=400
+    WIDTH = 600
+    HEIGHT = 400
 
+    Volumen = namedtuple('Volumen','width, height')
+    
     canvas = Canvas(window,width=WIDTH,height=HEIGHT)
     button_1 = Button(window, text="Ideal Gas")
     label_1 = Label(window,text="Ideal Gas")
 
-    label_1.grid(row=1,column=1)
-    button_1.grid(row=0,column=0)
+    label_1.grid(row=1, column=1)
+    button_1.grid(row=0, column=0)
     canvas.grid(row=0, column=1)
     
 
     number_of_particles=50
-    max_x_velocity=20
-    max_y_velocity=20
+    gas = Gas(0,number_of_particles,Volumen(width= WIDTH,height=HEIGHT),window,canvas)
+    particles = gas.create_gas()
 
-    radius = 5
-    Color = namedtuple('Color', 'red green blue')
-
-
-    initial_position_x=np.random.randint(10,WIDTH-1,number_of_particles)
-    x_velocity=np.random.randint(-max_x_velocity,max_x_velocity,number_of_particles)
-    initial_position_y=np.random.randint(10,HEIGHT-1,number_of_particles)
-    y_velocity=np.random.randint(-max_y_velocity,max_y_velocity,number_of_particles)
-
-    colors_depending_on_velocity=lambda x_vel, y_vel: Color(round((x_vel**2 + y_vel**2)**0.5*255/((max_y_velocity**2+max_x_velocity**2)**0.5)),139,116)
-
-    particles=[Ball(canvas,initial_position_x[i],initial_position_y[i],
-        x_velocity[i],y_velocity[i],radius,colors_depending_on_velocity(x_velocity[i],y_velocity[i])) for i in range(number_of_particles)]
-
-    while True:
-        for i in range(number_of_particles):
-            particles[i].move()
-
+    while True:        
+        gas.move_gas(particles)  
         window.update()
         time.sleep(0.01)
 
